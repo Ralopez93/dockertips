9cf609a823ae4fb91c2946a746243f58eb1c17af
Florido Paganelli <florido.paganelli@hep.lu.se>
2026-09-18 22:13:28 +0200

    Added DOCKEFILE-dev and scripts to setup the python environments

---
+++ scripts/setuppython2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pyenv global 2.7.18
+
+pip install virtualenv
+
+python -m virtualenv $TOOLSDAY_APPSDIR/myenvpy2.7
+
+source $TOOLSDAY_APPSDIR/myenvpy2.7/bin/activate
+
+pip install numpy
+pip install cowsay==3.0.0
---
+++ scripts/setuppython3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pyenv global 3.14.7
+
+python -m venv $TOOLSDAY_APPSDIR/myenvpy3.14
+
+source $TOOLSDAY_APPSDIR/myenvpy3.14/bin/activate
+
+pip install numpy
+pip install cowsay
