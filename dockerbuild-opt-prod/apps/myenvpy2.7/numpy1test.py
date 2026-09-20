4315e0e88ad705a9c52dc6d1041734ab712fa7ea
Florido Paganelli <florido.paganelli@hep.lu.se>
2026-09-20 22:46:39 +0200

    Adding different versions of the numpytest.py script to the production directory structure

---
+++ dockerbuild-opt-prod/apps/myenvpy2.7/numpy1test.py
@@ -0,0 +1,7 @@
+# Suggestion taken from <https://numpy.org/devdocs/numpy_2_0_migration_guide.html>
+
+import numpy as np
+
+myfloat = np.float32(3) + 3.
+
+print "Numpy version:", np.version.version, "myfloat value:", myfloat, "myfloat type:", type(myfloat)
